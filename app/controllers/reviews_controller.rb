369489671fa4c64694f5940282c074108cb1c3b1
Jonathan Parmley <jonathanparmley@gmail.com>
2021-04-14 21:19:56 +0200

    added review delete function

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -25,5 +25,5 @@ end
 private
 
 def review_params
-  params.require(:review).permit(:content)
+  params.require(:review).permit(:content, :rating)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_171255) do
+ActiveRecord::Schema.define(version: 2021_04_14_184322) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_04_13_171255) do
     t.integer "restaurant_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "rating"
     t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
